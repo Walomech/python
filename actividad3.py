f3410268d5d9aaccd714fe70cba0f4409487a5ac
Francisco Avendaño <fr.avendanom@duocuc.cl>
2026-05-14 11:24:24 -0400

    Update actividad3.py

--- actividad3.py
+++ actividad3.py
@@ -1,1 +1,19 @@
+#EJERCICIO 1
+print("CONTADORA DE PARTICIPANTES")
+print("__________________________")
 
+cant = int(input("Ingrese cantidad de participantes: "))
+
+nombres = []
+
+for i in range(cant):
+    nombre = input("Ingrese nombre del participante: ")
+    nombres.append(nombre)
+
+print(f"La cantidad de participantes es: {cant}")
+print("Los participantes son:")
+
+for nombre in nombres:
+    print(nombre)
+
+  
